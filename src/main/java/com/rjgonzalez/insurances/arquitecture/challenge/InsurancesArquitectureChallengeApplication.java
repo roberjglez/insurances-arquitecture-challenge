e8c9f6a8af173d23207c268fcadc7ad2f4032c99
rjgonzalez <rjgonzalez@ITEM-S59989.emea.msad.sopra>
2019-09-13 12:43:25 +0200

    Adding project, first commit

---
+++ src/main/java/com/rjgonzalez/insurances/arquitecture/challenge/InsurancesArquitectureChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.rjgonzalez.insurances.arquitecture.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsurancesArquitectureChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsurancesArquitectureChallengeApplication.class, args);
+	}
+
+}
